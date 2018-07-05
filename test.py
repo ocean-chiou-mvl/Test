a2922fcd4209ed805332508a6f38d7465cc5d24b
ocean <ocean@marvell.com>
2018-07-05 10:15:22 +0800

    git commit

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import os
 import datetime
-	
+a = 10
+b = 20
+
 if __name__ == "__main__":
-    print ("Hello World")
+	print ("The simple calculator")
+	print ("a = ", a)
+	print ("b = ", b)
+	print ("a + b", a + b)
